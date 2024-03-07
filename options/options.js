f375452141c1a04bd85b30969c26453d40e2ba99
Crowdbotics <team@crowdbotics.com>
2024-03-07 17:32:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-recipe-46858",
+  url: "https://yellow-recipe-46858.botics.co",
+  api: "https://yellow-recipe-46858.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
